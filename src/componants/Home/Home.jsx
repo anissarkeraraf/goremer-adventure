d0d373be95c966d84e96da1cb65c6225338f9e90
anissarkeraraf <anissarkeraraf@gmail.com>
2024-04-29 11:56:10 +0600

    added the advanture of json file

--- src/componants/Home/Home.jsx
+++ src/componants/Home/Home.jsx
@@ -1,15 +1,28 @@
 import { useLoaderData } from "react-router-dom";
 import Header from "../Header/Header";
 import TourSpots from "../tourSpots/TourSpots";
+import { useEffect, useState } from "react";
 
 
 const Home = () => {
     const tourist = useLoaderData();
     console.log(tourist)
+
+    const [adventure, setAdventure] = useState([]);
+
+    useEffect(() => {
+        fetch('/public/adventure.json')
+            .then(res => res.json())
+            .then(data => {
+                setAdventure(data)
+            })
+    }, [adventure])
+
+
     return (
-        <div className="w-11/12 mx-auto">
+        <div>
             <Header></Header>
-            <div>
+            <div className="w-11/12 mx-auto">
                 <div className=" my-10">
                     <h2 className="text-4xl text-center font-bold opacity-80"> Tourists Spots Section</h2>
                     <p className="text-center w-[450px] mx-auto mt-5 font-medium opaci75">Some famous places are mentioned in this section . You can see the details of these if you want , thanks</p>
@@ -20,10 +33,23 @@ const Home = () => {
                     }
                 </div>
             </div>
-            <div className="my-20">
-                <h2 className="text-4xl text-center">Most Popular Tour</h2>
+            <div className="my-20 divide-y-2 m-10">
                 <div>
-
+                    <h2 className="text-4xl font-medium text-center mb-5">DESTINATION</h2>
+                </div>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
+                    {adventure.map(ad => (
+                        <div className="card card-compact bg-base-100 shadow-xl">
+                            <figure><img src={ad.imgUrl} alt="Shoes" /></figure>
+                            <div className="card-body">
+                                <h2 className="card-title">Shoes!</h2>
+                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                <div className="card-actions justify-end">
+                                    <button className="btn btn-primary">Buy Now</button>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
--- src/componants/allTouristSpot/AllTouristSpot.jsx
+++ src/componants/allTouristSpot/AllTouristSpot.jsx
@@ -1,18 +1,34 @@
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import AllSpots from "./AllSpots";
 
-
 const AllTouristSpot = () => {
     const spots = useLoaderData();
-    // const { spotName, countryName, location, description, average, seasonality, travel, totalVisitors, email, userName, photoURL } = spots;
+    const [sortBy, setSortBy] = useState(""); 
+
+    const handleSort = (event) => {
+        setSortBy(event.target.value);
+    };
+
+    const sortedSpots = [...spots].sort((a, b) => {
+        if (sortBy === "ascending") {
+            return a.average - b.average;
+        }
+        return 0;
+    });
+
     return (
         <div className="w-11/12 mx-auto mb-10">
-            <h2 className="text-4xl text-center mt-10 mb-5 font-bold"> Southeast Asia Tourist Spot</h2>
-            <p className="text-center w-[600px] mx-auto mb-10">Southeast Asia is a popular tourist destination renowned for its stunning landmarks, historical sites, and natural wonders. </p>
+            <h2 className="text-4xl text-center mt-10 mb-5 font-bold">Southeast Asia Tourist Spot</h2>
+            <p className="text-center w-[600px] mx-auto mb-10">Southeast Asia is a popular tourist destination renowned for its stunning landmarks, historical sites, and natural wonders.</p>
+            <div className="mb-10 text-center">
+                <select id="sort" className="bg-[#33B249] p-2 rounded" onChange={handleSort}>
+                    <option className="bg-[#5ADBB5]" value="">Select</option>
+                    <option className="bg-[#5ADBB5]" value="ascending">Average</option>
+                </select>
+            </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
-                {
-                    spots.map(spot => <AllSpots key={spot._id} spot={spot}></AllSpots>)
-                }
+                {sortedSpots.map(spot => <AllSpots key={spot._id} spot={spot}></AllSpots>)}
             </div>
         </div>
     );
